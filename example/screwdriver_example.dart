3a6f745ce0ace051d58719484c023b691de43683
BirjuVachhani <brvachhani@gmail.com>
2020-08-16 19:02:08 +0530

    :tada: initial commit

---
+++ example/screwdriver_example.dart
@@ -0,0 +1,6 @@
+import 'package:screwdriver/screwdriver.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/screwdriver_test.dart
@@ -0,0 +1,16 @@
+import 'package:screwdriver/screwdriver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
