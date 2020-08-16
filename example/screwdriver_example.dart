3186125ba4c8d1ff0b64faf8efa59167c691d0a4
BirjuVachhani <brvachhani@gmail.com>
2020-08-16 19:39:43 +0530

    :recycle: remove boilerplate code :recycle: use effective_dart instead of pedantic

--- example/screwdriver_example.dart
+++ example/screwdriver_example.dart
@@ -1,6 +1,3 @@
-import 'package:screwdriver/screwdriver.dart';
-
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // TODO
 }
--- lib/screwdriver.dart
+++ lib/screwdriver.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library screwdriver;
-
-export 'src/screwdriver_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
