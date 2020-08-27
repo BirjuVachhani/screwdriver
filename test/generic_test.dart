a3c2c285ec6802d29671c67ce7e85c65f52cafc1
BirjuVachhani <brvachhani@gmail.com>
2020-08-27 17:15:38 +0530

    :white_check_mark: add generic tests

---
+++ test/generic_test.dart
@@ -0,0 +1,39 @@
+// Author: Birju Vachhani
+// Created Date: August 27, 2020
+
+import 'package:screwdriver/screwdriver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('scope functions tests', () {
+    expect([].apply((obj) => obj << 5), equals([5]));
+    expect([].apply(null), equals([]));
+    expect(5.run((obj) => (obj + 5).toString()), '10');
+    expect(5.run(null), null);
+  });
+
+  test('takeIf tests', () {
+    expect(5.takeIf((obj) => obj.isEven), null);
+    expect(5.takeIf((obj) => obj.isOdd), 5);
+    expect(5.takeIf(null), null);
+  });
+
+  test('takeUnless tests', () {
+    expect(45.takeUnless((obj) => obj.isEven), 45);
+    expect(45.takeUnless((obj) => obj.isOdd), null);
+    expect(45.takeUnless(null), null);
+  });
+
+  test('pairWith tests', () {
+    expect(15.pairWith('odd'), Pair(15, 'odd'));
+  });
+
+  test('TODO tests', () {
+    expect(() => TODO('unimplemented test'), throwsUnimplementedError);
+  });
+
+  test('run tests', () {
+    expect(run(() => int.parse('23') + 7), 30);
+    expect(run(null), null);
+  });
+}
