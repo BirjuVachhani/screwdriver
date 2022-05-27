27e2c19e8a40a5562fd82566d44a31fe69487351
birjuvachhani <brvachhani@gmail.com>
2022-05-27 22:02:24 +0530

    Add some typedefs

---
+++ lib/src/helpers/typedefs.dart
@@ -0,0 +1,13 @@
+typedef JsonMap = Map<String, dynamic>;
+
+typedef IntList = List<int>;
+
+typedef StringList = List<String>;
+
+typedef DoubleList = List<double>;
+
+typedef IntSet = Set<int>;
+
+typedef StringSet = Set<String>;
+
+typedef DoubleSet = Set<double>;
