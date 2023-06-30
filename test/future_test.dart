2fad082090472901e1c8eec21c218b5ed168a18a
birjuvachhani <brvachhani@gmail.com>
2023-06-30 22:37:54 +0530

    :white_check_mark: fix tests for runCaching function.

--- test/future_test.dart
+++ test/future_test.dart
@@ -13,13 +13,35 @@ void main() {
   });
 
   test('runCaching tests', () async {
-    await expectLater(runCaching(() async => int.parse('5')), completion(5));
-    await expectLater(
-        runCaching(() async => int.parse('abc')), throwsException);
     expect(runCaching(() => int.parse('5')), 5);
-    expect(runCaching(() => int.parse('5a')), null);
+    expect(runCaching(() => int.parse('5')), isNot(isA<Future<dynamic>>()));
+    expect(await runCaching(() => int.parse('5')), 5);
+    expect(() => runCaching(() => int.parse('abc')), isNot(throwsException));
+    expect(runCaching(() async => int.parse('abc')), completes);
+    expect(runCaching(() async => int.parse('abc')), completion(null));
+    expect(
+        runCaching(() => int.parse('abc'), onError: (_, __) => -1), equals(-1));
+    expect(runCaching(() async => int.parse('abc'), onError: (_, __) => -1),
+        completion(-1));
+    expect(runCaching(() => print('Hello')), isA<void>());
+    expect(runCaching(() => print('Hello')), isNot(isA<Future<dynamic>>()));
+    expect(runCaching(() => print('Hello')), isNot(throwsException));
+    expect(runCaching(() => throw Exception()), isNot(throwsException));
+
+    expect(
+        runCaching(() => throw Exception(),
+            onError: (_, __) => throw UnimplementedError()),
+        isNot(throwsException));
+    expect(
+        await runCaching(() async => throw Exception(),
+            onError: (_, __) => throw UnimplementedError()),
+        isNot(throwsException));
+
+    await expectLater(await runCaching(() => int.parse('5')), 5);
+    await expectLater(await runCaching(() => int.parse('5a')), null);
+
     Object? exception;
-    runCaching(() => int.parse('5a'),
+    await runCaching(() => int.parse('5a'),
         onError: (dynamic error, _) => exception = error);
     expect(exception, isA<FormatException>());
   });
