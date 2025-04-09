c39c8a073f0752f9c35bda36e511479bccf0830c
BirjuVachhani <brvachhani@gmail.com>
2025-04-09 10:46:48 +0530

    :white_check_mark: add tests for `containsAll`, `containsAny`, `containsNone` extensions for `Map`.

--- test/map_test.dart
+++ test/map_test.dart
@@ -22,6 +22,9 @@ void main() {
   test('toJson test', () {
     expect({'name': 'John'}.toJson(), equals('{"name":"John"}'));
     expect(<String, dynamic>{}.toJson(), equals('{}'));
+
+    expect({'name': 'John'}.toJsonString(), equals('{"name":"John"}'));
+    expect(<String, dynamic>{}.toJsonString(), equals('{}'));
   });
 
   test('except test', () {
@@ -84,5 +87,29 @@ void main() {
     expect({'a': 1, 'b': 2, 'c': 3}.findByValueOrNull(2)?.key, equals('b'));
     expect({'a': 1, 'b': 2, 'c': 3}.findByValueOrNull(2)?.value, equals(2));
     expect({'a': 1, 'b': 2, 'c': 3}.findByValueOrNull(4), isNull);
+  });
+
+  test('containsAll test', () {
+    expect({'a': 1, 'b': 2, 'c': 3}.containsAll(['a', 'b']), isTrue);
+    expect({'a': 1, 'b': 2, 'c': 3}.containsAll(['a', 'd']), isFalse);
+    expect({'a': 1, 'b': 2, 'c': 3}.containsAll([]), isTrue);
+    expect({}.containsAll([]), isTrue);
+    expect({}.containsAll(['a']), isFalse);
+  });
+
+  test('containsAny test', () {
+    expect({'a': 1, 'b': 2, 'c': 3}.containsAny(['a', 'b']), isTrue);
+    expect({'a': 1, 'b': 2, 'c': 3}.containsAny(['d', 'e']), isFalse);
+    expect({'a': 1, 'b': 2, 'c': 3}.containsAny([]), isFalse);
+    expect({}.containsAny([]), isFalse);
+    expect({}.containsAny(['a']), isFalse);
+  });
+
+  test('containsNone test', () {
+    expect({'a': 1, 'b': 2, 'c': 3}.containsNone(['d', 'e']), isTrue);
+    expect({'a': 1, 'b': 2, 'c': 3}.containsNone(['a', 'b']), isFalse);
+    expect({'a': 1, 'b': 2, 'c': 3}.containsNone([]), isTrue);
+    expect({}.containsNone([]), isTrue);
+    expect({}.containsNone(['a']), isTrue);
   });
 }
