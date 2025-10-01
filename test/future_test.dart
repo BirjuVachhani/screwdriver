5e8510fdf184768fc8673a6f11fa024575bd0b94
Birju Vachhani <brvachhani@gmail.com>
2025-10-01 21:09:34 +0530

    :white_check_mark: add tests for unawaited extension.

--- test/future_test.dart
+++ test/future_test.dart
@@ -19,29 +19,21 @@ void main() {
     expect(() => runCaching(() => int.parse('abc')), isNot(throwsException));
     expect(runCaching(() async => int.parse('abc')), completes);
     expect(runCaching(() async => int.parse('abc')), completion(null));
-    expect(
-        runCaching(() => int.parse('abc'), onError: (_, __) => -1), equals(-1));
-    expect(runCaching(() async => int.parse('abc'), onError: (_, __) => -1),
-        completion(-1));
+    expect(runCaching(() => int.parse('abc'), onError: (_, __) => -1), equals(-1));
+    expect(runCaching(() async => int.parse('abc'), onError: (_, __) => -1), completion(-1));
     expect(runCaching(() {}), isA<void>());
     expect(runCaching(() {}), isNot(throwsException));
     expect(runCaching(() => throw Exception()), isNot(throwsException));
 
-    expect(
-        runCaching(() => throw Exception(),
-            onError: (_, __) => throw UnimplementedError()),
-        isNot(throwsException));
-    expect(
-        await runCaching(() async => throw Exception(),
-            onError: (_, __) => throw UnimplementedError()),
+    expect(runCaching(() => throw Exception(), onError: (_, __) => throw UnimplementedError()), isNot(throwsException));
+    expect(await runCaching(() async => throw Exception(), onError: (_, __) => throw UnimplementedError()),
         isNot(throwsException));
 
     await expectLater(await runCaching(() => int.parse('5')), 5);
     await expectLater(await runCaching(() => int.parse('5a')), null);
 
     Object? exception;
-    await runCaching(() => int.parse('5a'),
-        onError: (dynamic error, _) => exception = error);
+    await runCaching(() => int.parse('5a'), onError: (dynamic error, _) => exception = error);
     expect(exception, isA<FormatException>());
   });
 
@@ -54,13 +46,38 @@ void main() {
     expect(() => someFuture(22).tryCatch(), returnsNormally);
     expect(() => someFuture(-1).tryCatch(), returnsNormally);
     expect(someFuture(20).tryCatch(), completion(equals((20, null))));
-    expect(
-        someFuture(-20).tryCatch(), completion(isA<(int?, TryCatchError)>()));
+    expect(someFuture(-20).tryCatch(), completion(isA<(int?, TryCatchError)>()));
 
     final (data, error) = await someFuture(-54).tryCatch();
     expect(data, isNull);
     expect(error, isNotNull);
     expect(error?.error, isArgumentError);
     expect(error?.stacktrace, isNotNull);
+  });
+
+  test('unawaited extension tests', () async {
+    // Test that unawaited doesn't throw and returns void
+    expect(() => someFuture(42).unawaited, returnsNormally);
+
+    // Test that unawaited works with futures that complete successfully
+    var completed = false;
+    someFuture(10).then((_) => completed = true).unawaited;
+    await Future.delayed(Duration(milliseconds: 10));
+    expect(completed, isTrue);
+
+    // Test that unawaited works with futures that throw errors
+    // The error should not be caught by the test
+    var errorHandled = false;
+    someFuture(-10).then((_) {}).catchError((e) {
+      errorHandled = true;
+    }).unawaited;
+    await Future.delayed(Duration(milliseconds: 10));
+    expect(errorHandled, isTrue);
+
+    // Test with post function
+    expect(() => post(() => 'test').unawaited, returnsNormally);
+
+    // Test with postDelayed function
+    expect(() => postDelayed(100, () => 'test').unawaited, returnsNormally);
   });
 }
